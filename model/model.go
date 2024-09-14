d9102d76d28a3c05512b4bd15da9facc5e7750eb
JPratama7 <josepratama080@gmail.com>
2024-09-15 00:17:04 +0700

    feat: add json tag

--- model/model.go
+++ model/model.go
@@ -15,18 +15,18 @@ type User struct {
 }
 
 type BlogPost struct {
-	ID        xid.ID    `db:"id"`
-	Title     string    `db:"title"`
-	Content   string    `db:"content"`
-	AuthorID  xid.ID    `db:"author_id"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID        xid.ID    `db:"id" json:"id"`
+	Title     string    `db:"title" json:"title"`
+	Content   string    `db:"content" json:"content"`
+	AuthorID  xid.ID    `db:"author_id" json:"author_id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
 
 type Comment struct {
-	ID        xid.ID    `db:"id"`
-	PostID    xid.ID    `db:"post_id"`
-	Author    string    `db:"author_name"`
-	Content   string    `db:"content"`
-	CreatedAt time.Time `db:"created_at"`
+	ID         xid.ID    `db:"id" json:"id"`
+	PostID     xid.ID    `db:"post_id" json:"post_id"`
+	AuthorName string    `db:"author_name" json:"author_id"`
+	Content    string    `db:"content" json:"content"`
+	CreatedAt  time.Time `db:"created_at" json:"created_at"`
 }
